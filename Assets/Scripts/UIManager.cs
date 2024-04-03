eaa72de0f570352158096faf66c3539a97512349
Robert Gillespie <robertarthurgillespie@outlook.com>
2024-04-03 15:31:19 -0500

    added text to speech plugin

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI simText;
+    public Slider textSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateTextSize()
+    {
+        simText.fontSize = textSlider.value;
+    }
+}
